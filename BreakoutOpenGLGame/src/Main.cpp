9d82f98740e567dba85ba66b7b7e6c71605202c2
Mental <Alekov99@mail.ru>
2024-09-14 15:00:11 +0500

    Add face culling

--- BreakoutOpenGLGame/src/Main.cpp
+++ BreakoutOpenGLGame/src/Main.cpp
@@ -42,6 +42,13 @@ int main()
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CCW);
+
+	//glEnable(GL_DEPTH_TEST);
+	//glDepthFunc(GL_LESS);
+
 	double deltaTime = 0.0f;
 	double lastFrameTime = 0.0f;
 
